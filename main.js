47021d80d74a37066afe32757663d61b7738e47c
pietro <pietro1990@hotmail.it>
2020-04-21 10:25:09 +0200

    migliorato

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 //chiedere quanti chilometri dovrà percorrere;
 var distanza = prompt("Quanti chilometri vuoi percorrere?");
 console.log( distanza );
+document.getElementById( "km" ) .innerHTML = distanza + " km";
 
 //chiedere quanti anni ha;
 var eta = prompt("Quanti anni hai?");
 console.log( eta );
+document.getElementById( "anni" ) .innerHTML = eta + " anni";
 
 //calcolo prezzo toltale del biglietto e visualizzazione;
+var molt = 0.21 * distanza;
+
 if (eta<18) {
     //calcolo del prezzo del biglietto in base ai chilometri per minorenni;
-    var molt = 0.21 * distanza;
-    var molt = molt * 20;
-    var divisione = molt / 100;
+    var molt2 = molt * 20;
+    var divisione = molt2 / 100;
+    var totale= molt - divisione;
     console.log( "tariffa ridotta per minorenni: " + divisione);
+    document.getElementById( "demo" ) .innerHTML = totale.toFixed(2) + " euro";
 }
 else if (eta>65) {
     //calcolo del prezzo del biglietto in base ai chilometri per anziani;
-    var molt = 0.21 * distanza;
-    var molt = molt * 40;
+    var molt2 = molt * 40;
     var divisione = molt / 100;
+    var totale= molt - divisione;
     console.log( "tariffa ridotta: " + divisione );
+    document.getElementById("demo").innerHTML = totale.toFixed(2) + " euro";
 }
 else {
     //calcolo del prezzo del biglietto in base ai chilometri tariffa piena;
     var molt = 0.21 * distanza;
     console.log( "tariffa intera: " + molt);
+    document.getElementById("demo").innerHTML = moltiplicazione .toFixed(2) + " euro";
 }
